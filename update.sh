f03a399fd47656eb5eee0d7f19d24a513f32ba62
Richard Liu <rtliu@mit.edu>
2019-08-24 20:16:31 -0400

    Adds Update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,3 @@
-# mpc
-
-```bash
 echo MIT username:
 read user
 ssh -t $user@athena.dialup.mit.edu '
@@ -8,10 +5,3 @@ cd /mit/mpc/web_scripts/site;
 svn update;
 bash -l
 '
-```
-
-## Past setup
-
-```bash
-svn checkout https://github.com/richardliutl/mpc/trunk/site
-```
