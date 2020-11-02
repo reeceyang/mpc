4a585839d3f1f8277a4fd517e9d8f5d73881692e
richardliutl <richardliutl@gmail.com>
2020-11-01 23:44:02 -0500

    Specifies charset

--- site/blog/prepare_post.lua
+++ site/blog/prepare_post.lua
@@ -2,6 +2,7 @@ local header_template_default = [[
 <!DOCTYPE html>
 <html lang="en-us" style="" class=" no-touchevents">
    <head>
+      <meta charset="utf-8" />
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5">
