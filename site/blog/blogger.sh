d9a11a068cb00abb1771fde776e32353407b3312
richardliutl <richardliutl@gmail.com>
2020-11-01 23:32:20 -0500

    Adds image lightbox, newsletter text

--- site/blog/blogger.sh
+++ site/blog/blogger.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")"
 pandoc="$(pwd)"/../../tools/bin/pandoc
 unzip -o \*.zip;
-links="# links:\n"
+links="Hi MPC!\n\nOn behalf of the exec board, we are super excited to announce the start of our club's very own Newsletter. This idea began with the goal of finding ways to engage and connect with the community remotely, as we continue to adapt to the challenges of online learning. For our first issue, we've put together a fun variety of articles. Some of which showcase student work, while others focus on exploring music at large, including a spotify playlist curated by exec. The articles have all been uploaded to our newly remodeled website. Below is a list of embedded links to the respective articles. We hope you enjoy this small taste of what the community has been up to so far this semester!\n\n"
 for f in formatted\ for\ website/*.docx;
     do basefname=$(basename "$f" .docx);
     title=$(python3 blog.py lt -t "$basefname");
@@ -11,10 +11,11 @@ for f in formatted\ for\ website/*.docx;
 	$pandoc --lua-filter prepare_post.lua "$f" -o "$fname".md --extract-media="media-$fname";
 	$pandoc --lua-filter prepare_post.lua "$fname".md -o "$fname".html \
     	--data-dir="media-$fname" --metadata title="$title" --metadata author="$author";
-    links="${links} * [http://mpc.mit.edu/blog/$fname.html](http://mpc.mit.edu/blog/$fname.html)\n";
+    links="${links} * [$title](http://mpc.mit.edu/blog/$fname.html) by $author\n";
 done;
-echo -e "${links}" > links.md;
-$pandoc -s -c ../assets/css/blog.css links.md -o links.html;
+echo -e "${links}\nAs we continue to build on this new venture, the structure of future newsletters, as well as our website, are still works in progress. If you are interested in contributing, especially in the form writing any music-related content for future issues, let us know at mit-mpc-exec@mit.edu. If you have any feedback, comments, questions, suggestions etc, we would love to hear them as well.\n\nHave a great week!\n\n\- MPC exec"\
+    > links.md;
+$pandoc --lua-filter prepare_post.lua links.md -o NovemberNewsletter.html --metadata title="November Newsletter" --metadata header="links";
 
 
 	# $pandoc --lua-filter prepare_post.lua "$f" -o "$fname".html \
--- site/blog/prepare_post.lua
+++ site/blog/prepare_post.lua
@@ -1,4 +1,4 @@
-local header_template = [[
+local header_template_default = [[
 <!DOCTYPE html>
 <html lang="en-us" style="" class=" no-touchevents">
    <head>
@@ -37,6 +37,42 @@ local header_template = [[
    <div class="Post Container">
 ]]
 
+local header_template_links = [[
+<!DOCTYPE html>
+<html lang="en-us" style="" class=" no-touchevents">
+   <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5">
+      <title>${title} - MIT MPC</title>
+      <link rel="stylesheet" href="${homePath}/assets/css/blog.css">
+   </head>
+   <body class="Body--post">
+
+<div class="Header">
+   <div class="Header-content">
+      <div class="Header-top">
+         <a class="Logo" href="${homePath}/" aria-label="MIT MPC, back to homepage">
+            <div class="Logo-textBigLetter"> <span aria-hidden="true"> 
+               MIT Music Production Collaborative
+            </span> </div> </a>
+         <div class="Menu">
+            <a class="Menu-link " href="${homePath}/index.html"> About </a>
+               <span aria-hidden="true">/</span>
+            <a class="Menu-link " href="${homePath}/news.html"> Archive </a>
+         </div>
+      </div>
+      <h1 class="Header-title Header-title--page" style="transform: translateY(0px); opacity: 1;"> <span aria-hidden="true">
+         ${title}
+         </span>
+      </h1>
+   </div>
+   <div class="Header-angle Header-angle--left"></div>
+   <div class="Header-angle Header-angle--right"></div>
+</div>
+   <div class="Post Container">
+]]
+
 local metavars = {
     imagePath = "",
     homePath = "..",
@@ -56,18 +92,71 @@ end
 
 -- Filter with these function if the target format is HTML
 if FORMAT:match 'html' then
-  -- Append header template
-  function Pandoc (doc)
-      local meta = doc.meta
-      local blocks = doc.blocks
-      -- Replacing placeholders with their metadata value using interp
-      blocks:insert(1, pandoc.RawBlock('html', interp(header_template, metavars) ))
-      pandoc.walk_block(doc.blocks[1], {replace})
-      return pandoc.Pandoc(blocks, meta)
+  local lightboxes = { }
+  local thumbnail_template = [[
+    <!-- The link that, when clicked, will display the image in full screen -->
+    <a href="#${id}">
+      <img src="${src}" alt="Thumbnail" style="width:${width}; height:${height}; object-fit: contain;">
+    </a>
+  ]]
+  local captioned_thumbnail_template = [[
+    <figure>
+      <a href="#${id}">
+        <img src="${src}" alt="Thumbnail" style="width:${width}; height:${height}; object-fit: contain;">
+      </a>
+      <figcaption aria-hidden="true">${caption}
+      </figcaption>
+    </figure>
+  ]]
+  local lightbox_template = [[
+    <!-- The full screen image, hidden by default  -->
+    <a href="#_" class="lightbox" id="${id}">
+      <img src="${src}" alt="Fullscreen">
+    </a>
+  ]]
+
+  function Image (elem)
+    -- Use object-fit style to align image with screen size
+    local vars = {
+      width = elem.attributes['width'],
+      height = 'auto',
+      -- height = elem.attributes['height'],
+      id = pandoc.utils.sha1(elem.src),
+      caption = pandoc.utils.stringify(elem.caption),
+      src = elem.src,
+    }
+    local thumbnail = interp(thumbnail_template, vars)
+    if #vars['caption'] > 0 then
+      thumbnail = interp(captioned_thumbnail_template, vars)
+    end
+    local lightbox = interp(lightbox_template, vars)
+    
+    lightboxes[#lightboxes+1] = lightbox -- Add lightbox to end
+    return pandoc.RawInline('html', thumbnail)
   end
 
+  -- Set header template according to metadata
+  function select_header_template()
+    if metavars['header'] == 'links' then
+      return header_template_links
+    end
+    return header_template_default
+  end
+
+  -- Append header template
+  function Pandoc (doc)
+    local meta = doc.meta
+    local blocks = doc.blocks
+    -- Replacing placeholders with their metadata value using interp
+    local header_template = select_header_template()
+    lightboxes[#lightboxes+1] = interp(header_template, metavars)
+    blocks:insert(1, pandoc.RawBlock('html', table.concat(lightboxes,"\n")))
+    pandoc.walk_block(doc.blocks[1], {replace})
+    return pandoc.Pandoc(blocks, meta)
+  end
+
+  -- If Para starts with iframe tag, escape the Para to render content correctly
   function Para (elem)
-    -- If Para starts with iframe tag, escape the Para to render content correctly
     local content = pandoc.utils.stringify(elem.content)
     local iframe = '<iframe'
     if content:sub(1, #iframe) == iframe then
@@ -88,17 +177,6 @@ if FORMAT:match 'html' then
      else
         return tostring(o)
      end
-  end
-
-  function Image (elem)
-    -- Use object-fit style to align image with screen size
-    local vars = {
-      width = elem.attributes['width'],
-      height = elem.attributes['height']
-    }
-    elem.attributes.style = interp('width:${width}; height:${height}; object-fit: contain;', vars)
-    -- elem.caption = {pandoc.Str "Hello, World"}
-    return elem
   end
 end
 
@@ -122,7 +200,7 @@ if FORMAT:match 'markdown' then
         blocks[i-1].content[1].caption = blocks[i].content
         -- sets dimensions
         blocks[i-1].content[1].attributes['width'] = '100%'
-        blocks[i-1].content[1].attributes['height'] = '100%'
+        blocks[i-1].content[1].attributes['height'] = 'auto'
         blocks:remove(i)
       end
     end
