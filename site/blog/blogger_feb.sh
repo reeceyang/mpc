f3697095e058be4cd6a1bc53a7c3bdad7643caf0
richardliutl <richardliutl@gmail.com>
2021-02-22 23:37:02 -0500

    Updates February newsletter and metadata

--- site/blog/blogger_feb.sh
+++ site/blog/blogger_feb.sh
@@ -13,7 +13,7 @@ for f in February\ 2021/*.docx;
     	--data-dir="media-$fname" --metadata title="$title" --metadata author="$author";
     links="${links} * [$title](http://mpc.mit.edu/blog/$fname.html) by $author\n";
 done;
-echo -e "${links}\nAs we continue to build on this new venture, the structure of future newsletters, as well as our website, are still works in progress. If you are interested in contributing, especially in the form of writing any music-related content for future issues, let us know at mit-mpc-exec@mit.edu. If you have any feedback, comments, questions, suggestions etc, we would love to hear them as well.\n\nHave a great week!\n\n\- MPC exec"\
+echo -e "${links}\nAs we continue to build on this new venture, the structure of future newsletters, as well as our website, are still works in progress. If you are interested in contributing, especially in the form of writing any music-related content for future issues, let us know at [mit-mpc-exec@mit.edu](mail-to:mit-mpc-exec@mit.edu). If you have any feedback, comments, questions, suggestions etc, we would love to hear them as well.\n\nHave a great week!\n\n\- MPC exec"\
     > links.md;
 $pandoc --lua-filter prepare_post.lua links.md -o FebruaryNewsletter.html --metadata title="February Newsletter" --metadata header="links";
 
