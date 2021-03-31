7968c78d0ee05824d2a3bb6ee4037b76d6f988f6
richardliutl <richardliutl@gmail.com>
2021-03-30 22:07:30 -0400

    Adds video tutorial to march newsletter

--- site/blog/blogger_march.sh
+++ site/blog/blogger_march.sh
@@ -1,7 +1,7 @@
 cd "$(dirname "$0")"
 pandoc="$(pwd)"/../../tools/bin/pandoc
 # unzip -o \*.zip; # Already unzipped in blogger.sh
-links="Hi MPC!\n\nWelcome to our March newsletter. For this issue, we’ve answered a lot of the awesome questions submitted to us by some of you earlier this month. If you’re seeking advice on a particular topic, or just have any questions that you’d like to see answered in a future newsletter, ask them anonymously [here](https://docs.google.com/forms/d/e/1FAIpQLScn6SljlVo-juUKYFvcssG8-8tXYiLI2yhE1M-38cCN2hAK8w/viewform), or email us at [mit-mpc-exec@mit.edu](mailto:mit-mpc-exec@mit.edu).\nAnother exciting development in this month’s issue is the first of our new series of video tutorials on the MPC youtube channel! Below is a list of embedded links to this month’s content:\n\n"
+links="Hi MPC!\n\nWelcome to our March newsletter. For this issue, we’ve answered a lot of the awesome questions submitted to us by some of you earlier this month. If you’re seeking advice on a particular topic, or just have any questions that you’d like to see answered in a future newsletter, ask them anonymously [here](https://docs.google.com/forms/d/e/1FAIpQLScn6SljlVo-juUKYFvcssG8-8tXYiLI2yhE1M-38cCN2hAK8w/viewform), or email us at [mit-mpc-exec@mit.edu](mailto:mit-mpc-exec@mit.edu).\nAnother exciting development in this month’s issue is the first of our new series of video tutorials on the MPC youtube channel! Below is a list of embedded links to this month’s content:\n\n * [VIDEO TUTORIAL: DAW Basics](https://www.youtube.com/watch?v=hCe1qO9OVk8) by Willy Wu\n"
 for f in March\ 2021/*.docx;
     do basefname=$(basename "$f" .docx);
     title=$(python3 blog.py lt -t "$basefname");
